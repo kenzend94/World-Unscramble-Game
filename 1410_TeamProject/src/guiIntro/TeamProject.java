6ae4dd2e31b205668ffe26e0bb7b6c52368d5c29
kenze <kenze@ACER-LAPTOP>
2020-04-06 08:45:23 -0600

    Add menu

--- 1410_TeamProject/src/guiIntro/TeamProject.java
+++ 1410_TeamProject/src/guiIntro/TeamProject.java
@@ -4,11 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class TeamProject extends JFrame {
-
+	
 	private JPanel contentPane;
 
 	/**
@@ -32,11 +35,42 @@ public class TeamProject extends JFrame {
 	 */
 	public TeamProject() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 584, 402);
+		
+		JMenuBar menuBar = menuBar();
+		setJMenuBar(menuBar);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+	}
+
+	private JMenuBar menuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menuFile = new JMenu("File");
+		
+		JMenuItem File_NewGame = new JMenuItem("New Game");
+		JMenuItem File_Open = new JMenuItem("Open");
+		JMenuItem File_Save = new JMenuItem("Save");
+		JMenuItem File_Exit = new JMenuItem("Exit");
+		
+		menuFile.add(File_NewGame);
+		menuFile.add(File_Open);
+		menuFile.add(File_Save);
+		menuFile.add(File_Exit);
+		
+		JMenu menuOptions = new JMenu("Options");
+		
+		JMenuItem Options_UserRanks = new JMenuItem("User Ranks");
+		JMenuItem Options_ChangBackground = new JMenuItem("Change Background");
+		
+		menuOptions.add(Options_UserRanks);
+		menuOptions.add(Options_ChangBackground);
+		
+		menuBar.add(menuFile);
+		menuBar.add(menuOptions);
+		return menuBar;
 	}
 
 }
